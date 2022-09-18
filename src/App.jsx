9851308e4a3d0e58ff6de269b5ed83521b3cdfe0
Edio Gama <ediogama@hotmail.com>
2022-09-17 22:17:28 -0300

    add css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+
+import './styles.css';
 
 export function App() {
-  return (
-    <div>
-      <Post author="Edio Gama" content="a great post" />
-      <Post author="Vanessa Vital" content="another post" />
+    return (
+        <div>
+            <Header />
+            <Post author="Edio Gama" content="a great post" />
+            <Post author="Vanessa Vital" content="another post" />
 
-    </div>
-  )
+        </div>
+    )
 }
